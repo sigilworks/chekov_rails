dc0e500c56e607f44db822c298306632fcbdaf52
sigilworks <tj@sigilworks.com>
2013-07-19 08:59:42 -0400

    Changed date format in comments to human readable ones.
    Signed-off-by: sigilworks <tj@sigilworks.com>

--- chekov/app/presenters/commentviewpresenter.rb
+++ chekov/app/presenters/commentviewpresenter.rb
@@ -1,6 +1,8 @@
 require 'taskrowpresenter'
 
 class CommentViewPresenter
+  include ActionView::Helpers::DateHelper
+
   attr_reader :task
 
   def initialize(comment, task)
@@ -18,7 +20,8 @@ class CommentViewPresenter
 
   def comment_date
     # Jul 12, 2013 2:09 PM
-  	@comment.created_at.strftime('%b %d, %Y  %I:%M %p')
+  	# @comment.created_at.strftime('%b %d, %Y  %I:%M %p')
+    distance_of_time_in_words_to_now(@comment.created_at, :include_seconds => true) 
   end
 
   # method missing to delegate to @user or super
