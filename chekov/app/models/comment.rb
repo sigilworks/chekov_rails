7fa3a2d4043e417b64dbafdde11dca542e915d4e
sigilworks <tj@sigilworks.com>
2013-07-25 10:30:01 -0400

    Fixed the new comment -> task issue with :update_only param to accepts_nested_attributes_for

--- chekov/app/models/comment.rb
+++ chekov/app/models/comment.rb
@@ -1,14 +1,15 @@
 class Comment < ActiveRecord::Base
+
   belongs_to :task, :autosave => true, :touch => true
-  belongs_to :commenter, class_name: "User"
+  belongs_to :commenter, :class_name => "User"
 
-  accepts_nested_attributes_for :task
+  accepts_nested_attributes_for :task, :update_only => true
 
-  validates :task, 
-  			:presence =>  { :message => "Every comment must be associated with a task!" }, 
+  validates :task,
+  			:presence =>  { :message => "Every comment must be associated with a task!" },
   			:on => :save
 
-  validates :commenter, 
+  validates :commenter,
   			:presence => { :message => "Every comment must be associated with a commenter!" },
   			:on => :save
 
--- chekov/app/models/task.rb
+++ chekov/app/models/task.rb
@@ -1,9 +1,11 @@
 class Task < ActiveRecord::Base
+
   belongs_to :application
   belongs_to :reporter, class_name: "User"
   belongs_to :assignee, class_name: "User"
   belongs_to :status
   belongs_to :browser
+
   has_many   :comments, :dependent => :destroy
 
   validates :application,
