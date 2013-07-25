9b2f64452fdd6fe5d008c946ae6ddd1ba66d2ca4
sigilworks <tj@sigilworks.com>
2013-07-25 16:45:46 -0400

    Adding a TODOS list in /storage

--- chekov/app/services/filteredviewstrategy.rb
+++ chekov/app/services/filteredviewstrategy.rb
@@ -3,6 +3,23 @@ require 'singleton'
 class FilteredViewStrategy
   include Singleton
 
+  class << self
+
+    def for(user)
+
+    end
+
+    # def filtered_tasks(filter)
+    #   case filter
+    #   when 'all'
+    #     Task.all.order(:updated_at => :desc)
+    #   when *Status.all.map(&:shortname)
+    #     Task.where(:status_id => Status.where(:shortname => filter)).order(:updated_at => :desc)
+    #   when 'mine'
+    #     Task.where(:assignee => current_user).order(:updated_at => :desc)
+    #   end
+    # end
 
 
+  end
 end
---
+++ chekov/storage/TODOS.rb
@@ -0,0 +1,7 @@
+
+ 1. Edit Task
+ 		- anyone can edit tasks (and shouldnt we track this somewhere?)
+ 2.	Edit Comment
+ 		- only admins && commenter can edit comments
+ 		- only admins && commenter can delete comments
+ 		(^^ probably need a Permissions/RoleService or -Strategy for these too)
