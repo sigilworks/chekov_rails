c81f3589e4b712caf65e27fcb0d0d0c00cc6f032
sigilworks <tj@sigilworks.com>
2013-08-08 17:35:39 -0400

    Added a TODO for the next step in the strategy to get this all working properly (without resorting to some plug'n'play gem!)

--- chekov/app/models/concerns/enumable.rb
+++ chekov/app/models/concerns/enumable.rb
@@ -9,6 +9,15 @@ module Enumable
       define_singleton_method(name.to_sym) { klass.where(:name => name.to_s.upcase).first.id }
     end
 
+    # TODO:
+    # *instead*, let's make one db call at startup, move ids->vals to a hash,
+    # then use hash to make the attributes on the class:
+    # e.g., Hash[ Status.pluck(:name, :id)  ]
+    # => { 'CLOSED' => 1, 'OPEN' => 2, 'NEW' => 3, 'URGENT' => 4, 'REOPENED' => 5, 'AWAITING_FEEBACK' => 6 }, &c.
+    # Hash[klass.pluck(:name, :id)].each do |name, id|
+    #   define_singleton_method(name.to_sym) { id }
+    # end
+
     def <=>(other)
       klass = self.class.name.constantize
       if other.is_a? klass
