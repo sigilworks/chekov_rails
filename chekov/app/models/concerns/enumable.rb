d6133742e7241e02496c9898d298baffc587f1c8
sigilworks <tj@sigilworks.com>
2013-08-07 22:32:32 -0400

    Fixing some metaprogramming issues.
    Signed-off-by: sigilworks <tj@sigilworks.com>

--- chekov/app/models/concerns/enumable.rb
+++ chekov/app/models/concerns/enumable.rb
@@ -3,12 +3,10 @@ module Enumable
 
   included do
     # get class name that this is being mixed into...
-    CLASS_NAME = name.constantize
-    class << self
-      # create dynamic assignment of constants on eigenclass
-      CLASS_NAME.all.map(&:name).each do |name|
-        define_method(name.to_sym) { CLASS_NAME.where(:name => name.to_s.upcase).first.id }
-      end
+    klass = self.name.constantize
+    # create dynamic assignment of constants on eigenclass
+    klass.all.map(&:name).each do |name|
+      define_singleton_method(name.to_sym) { klass.where(:name => name.to_s.upcase).first.id }
     end
   end
 end
--- chekov/app/services/role_permissions_strategy.rb
+++ chekov/app/services/role_permissions_strategy.rb
@@ -6,6 +6,10 @@ module RolePermissionsStrategy
     RolePermissionsMatrix.new(user)
   end
 
+  # Permission:: 'ADMIN', 'READ_ONLY', 'ADD_ONLY'
+  # Role:: 'ASSIGNEE', 'REPORTER', 'COMMENTER', 'ANY'
+  # Team:: 'PRODUCT', 'DEVELOPMENT', 'OPS', 'MGMT', 'OTHER'
+
   class RolePermissionsMatrix
 
     def initialize(user)
@@ -13,29 +17,45 @@ module RolePermissionsStrategy
       @permission, @role, @team = user.permission, user.role, user.team
     end
 
+    # New Task:
+    #   - anyone (non-readonly) can enter new tasks
     def new_task?
+      is_all_except_readonly?
     end
-
+    # Edit Task:
+    #   - anyone (non-readonly) can edit tasks (and shouldnt we track this somewhere?)
     def edit_task?(taskid)
+      is_all_except_readonly?
     end
-
+    # Assign/Change assignation of Task:
+    #   - only admin
     def change_assignee?(taskid)
+      is_admin?
     end
-
+    # Change status of Task:
+    #   - anyone (non-readonly, non-addonly)
     def change_status?(taskid)
+      is_all_except_readonly_addonly?
     end
-
+    # Delete Task:
+    #   - only admin
     def delete_task?(taskid)
+      is_admin?
     end
-
+    # New Comment:
+    #   - anyone (non-readonly) can enter new comments
     def new_comment?
+      is_all_except_readonly?
     end
-
+    # Edit Comment:
+    #   - only admins && commenter can edit comments
     def edit_comment?(commentid)
       is_admin_or_commenter?(commentid)
     end
-
+    # Delete Comment
+    #   - only admins && commenter can delete comments
     def delete_comment?(commentid)
+      is_admin_or_commenter?(commentid)
     end
 
   private
@@ -57,31 +77,5 @@ module RolePermissionsStrategy
     end
 
   end
-
-  # Permission:: 'ADMIN', 'READ_ONLY', 'ADD_ONLY'
-  # Role:: 'ASSIGNEE', 'REPORTER', 'COMMENTER', 'ANY'
-  # Team:: 'PRODUCT', 'DEVELOPMENT', 'OPS', 'MGMT', 'OTHER'
-
-  #######  TASKS  #######
-  # New Task:
-  #   - anyone (non-readonly) can enter new tasks
-  # Edit Task:
-  #   - anyone (non-readonly) can edit tasks (and shouldnt we track this somewhere?)
-  # Assign/Change assignation of Task:
-  #   - only admin
-  # Change status of Task:
-  #   - anyone (non-readonly, non-addonly)
-  # Delete Task:
-  #   - only admin
-
-  ########  COMMENTS  #######
-  # New Comment:
-  #   - anyone (non-readonly) can enter new comments
-  # Edit Comment:
-  #   - only admins && commenter can edit comments
-  # Change status of Task from Comment:
-  #   - anyone (non-readonly, non-addonly)
-  # Delete Comment
-  #   - only admins && commenter can delete comments
 
 end
