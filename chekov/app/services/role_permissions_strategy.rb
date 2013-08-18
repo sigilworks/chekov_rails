19a6e49cce71bed15e94e31e5daa91cd7f218b2a
sigilworks <tj@sigilworks.com>
2013-08-18 19:28:28 -0400

    Starting to tweak RolePermissionsStrategy
    Signed-off-by: sigilworks <tj@sigilworks.com>

--- chekov/app/services/role_permissions_strategy.rb
+++ chekov/app/services/role_permissions_strategy.rb
@@ -20,43 +20,42 @@ module RolePermissionsStrategy
       @permission, @roles, @team = user.permission, user.roles, user.team
     end
 
-    # New Task:
-    #   - anyone (non-readonly) can enter new tasks
+    # New Task: anyone (non-readonly) can enter new tasks
     def new_task?
       is_all_except_readonly?
     end
-    # Edit Task:
-    #   - anyone (non-readonly) can edit tasks (and shouldnt we track this somewhere?)
+
+    # Edit Task: anyone (non-readonly) can edit tasks (and shouldnt we track this somewhere?)
     def edit_task?(taskid)
       is_all_except_readonly?
     end
-    # Assign/Change assignation of Task:
-    #   - only admin
+
+    # Assign/Change assignation of Task: only admin
     def change_assignee?(taskid)
       is_admin?
     end
-    # Change status of Task:
-    #   - anyone (non-readonly, non-addonly)
+
+    # Change status of Task: anyone (non-readonly, non-addonly)
     def change_status?(taskid)
       is_all_except_readonly_addonly?
     end
-    # Delete Task:
-    #   - only admin
+
+    # Delete Task: only admin
     def delete_task?(taskid)
       is_admin?
     end
-    # New Comment:
-    #   - anyone (non-readonly) can enter new comments
+
+    # New Comment: anyone (non-readonly) can enter new comments
     def new_comment?
       is_all_except_readonly?
     end
-    # Edit Comment:
-    #   - only admins && commenter can edit comments
+
+    # Edit Comment: only admins && commenter can edit comments
     def edit_comment?(commentid)
       is_admin_or_commenter?(commentid)
     end
-    # Delete Comment
-    #   - only admins && commenter can delete comments
+
+    # Delete Comment: only admins && commenter can delete comments
     def delete_comment?(commentid)
       is_admin_or_commenter?(commentid)
     end
