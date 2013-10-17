4c929af3493189079684921253b7464a113fe8fe
sigilworks <tj@sigilworks.com>
2013-10-17 17:23:28 -0400

    Removing mutable state out of the TaskFilterService

--- chekov/app/services/task_filter_service.rb
+++ chekov/app/services/task_filter_service.rb
@@ -5,33 +5,38 @@ module TaskFilterService
   ALL_FILTER_NAMES = (Status.all.map(&:shortname) + Filter.custom_filters).map(&:downcase) << 'all'
 
   def with_tasks(tasks)
-    @tasks = tasks
-    self
+    TaskFilterParams.new(tasks)
   end
 
-  def with_filter(filter)
-    @filter = ALL_FILTER_NAMES.include?(filter) ? Filter.new(filter) : Filter.new(:all)
-    self
-  end
+  class TaskFilterParams
+    def initialize(tasks)
+      @tasks = tasks
+    end
 
-  def for_user(user)
-    @user = user || User.nobody
-    self
-  end
+    def with_filter(filter)
+      @filter = ALL_FILTER_NAMES.include?(filter) ? Filter.new(filter) : Filter.new(:all)
+      self
+    end
 
-  def filtered_tasks
-    case @filter.name 
-    when 'all'
-      @tasks
-    when *Status.all.map(&:shortname)
-      @tasks.where status_id: Status.find_by(:shortname => @filter.name)
-    when 'mine'
-      @tasks.where assignee: @user
-    when 'unassigned'
-      @tasks.where assignee: User.nobody
-    when 'active'
-      @tasks.where status_id: Status.active
+    def for_user(user)
+      @user = user || User.nobody
+      self
+    end
+
+    def filtered_tasks
+      case @filter.name
+      when 'all'
+        @tasks
+      when *Status.all.map(&:shortname)
+        @tasks.where status_id: Status.find_by(:shortname => @filter.name)
+      when 'mine'
+        @tasks.where assignee: @user
+      when 'unassigned'
+        @tasks.where assignee: User.nobody
+      when 'active'
+        @tasks.where status_id: Status.active
+      end
     end
   end
-    
+
 end
